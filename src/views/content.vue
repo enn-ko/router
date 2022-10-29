6c4d60a08d7c159dec912a496d9a356a6198a9b1
enn-ko <naneimonkhaing7@gmail.com>
2022-10-29 15:36:39 +0630

    test-vuerouter

---
+++ src/views/content.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="content">
+        <h2>This is content</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro adipisci, beatae eaque ut at pariatur veniam. Dolores, necessitatibus laboriosam eius exercitationem doloremque earum? Assumenda veritatis corporis quas nostrum distinctio? Eveniet.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style >
+
+</style>
